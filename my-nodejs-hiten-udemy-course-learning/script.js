b06293a8427748f9574a793dd83bddd314ce222d
VanshSh <vanshsharma9354@gmail.com>
2026-01-07 23:47:03 +0530

    Add initial project files including .gitignore, README, package.json, and script.js

---
+++ my-nodejs-hiten-udemy-course-learning/script.js
@@ -0,0 +1,10 @@
+console.log('😇 My name is Vansh  in script.js=> ')
+const fs = require('fs')
+/**
+ * What is node: in (node:fs)? Explain everything about using the "node:" prefix for built-in modules.
+ *
+ * Important methods and usages of the fs (file system) module in Node.js.
+ *
+ * Also what is {recursive: true}
+ *
+ */
