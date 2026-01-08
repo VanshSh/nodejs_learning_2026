98dfeea9058f50f56bd2576a3d8d87b32dcab003
VanshSh <vanshsharma9354@gmail.com>
2026-01-09 00:11:33 +0530

    Update README.md with learning notes on Event Loop, Event Queue, and Thread Pool; simplify script.js comments

--- my-nodejs-hiten-udemy-course-learning/script.js
+++ my-nodejs-hiten-udemy-course-learning/script.js
@@ -1,10 +1,6 @@
 console.log('😇 My name is Vansh  in script.js=> ')
 const fs = require('fs')
 /**
- * What is node: in (node:fs)? Explain everything about using the "node:" prefix for built-in modules.
- *
- * Important methods and usages of the fs (file system) module in Node.js.
- *
- * Also what is {recursive: true}
+ * Event Queue , Event Loop, Thread Pool
  *
  */
