9a56ffdfeffff02f2981954fc6a38bf0c38b369e
VanshSh <vanshsharma9354@gmail.com>
2026-01-09 22:45:52 +0530

    Add myEvents.js file with EventEmitter examples and usage

---
+++ my-nodejs-hiten-udemy-course-learning/01_events/myEvents.js
@@ -0,0 +1,43 @@
+const EventEmitter = require('events')
+
+const eventEmitter = new EventEmitter()
+
+eventEmitter.on('greet', (name) => {
+  console.log('😇 Hello Event Emitter', name)
+})
+
+eventEmitter.once('pushNotify', () => {
+  console.log('😇 This will run only once ')
+})
+
+const myListener = () => {
+  console.log('😇 This is another test listener ')
+}
+
+eventEmitter.on('test', myListener)
+
+// Emit the event
+eventEmitter.emit('greet', 'Vansh')
+eventEmitter.emit('greet', 'Anuj')
+eventEmitter.emit('pushNotify')
+eventEmitter.emit('pushNotify') // it will not run
+eventEmitter.emit('test')
+eventEmitter.removeListener('test', myListener)
+eventEmitter.emit('test') // it will not run
+console.log('😇 L-26 in myEvents.js=> ')
+console.log(eventEmitter.listeners('greet'))
+
+// Class Approach
+
+class Chat extends EventEmitter {
+  sendMessage(message) {
+    this.emit('messageReceived', message)
+  }
+}
+
+const chat = new Chat()
+chat.on('messageReceived', (msg) => {
+  console.log('New Message: ', msg)
+})
+
+chat.sendMessage('Hello! How are you?')
